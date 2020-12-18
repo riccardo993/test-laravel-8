4a4ebe15d9f5391d97e4b3f9b48473bb2b32cfb2
RiccardoGaleazzi <glr93@hotmail.it>
2020-12-18 22:57:31 +0100

    New migration

---
+++ database/migrations/2020_12_18_213913_create_media_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('link');
+            $table->string('media');
+            $table->timestampTz('taken_at');
+            $table->text('description');
+            $table->timestamp('published_at')->nullable();
+            $table->string('author');
+            $table->string('author_id');
+            $table->string('tags')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
